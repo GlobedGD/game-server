b08aeef4fc67ee613bbb63216ab587073465ad73
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-12-21 00:09:45 +0100

    ok

--- docker-build-inner.sh
+++ docker-build-inner.sh
@@ -1,6 +1,6 @@
 mv .cargo .cargo-temp
 rustup toolchain add nightly
 rustup override set nightly
-RUSTFLAGS="--cfg tokio_unstable" SERVER_SHARED_PREBUILT_DATA=1 cargo build --release
+SERVER_SHARED_PREBUILT_DATA=1 cargo build --release
 mv .cargo-temp .cargo
 cp target/release/game-server /work/game-server-x64
