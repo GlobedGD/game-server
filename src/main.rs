aeca73ee2fc8b02db5e1cd125b1b5a8a0381afe7
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-01-07 20:55:06 +0100

    don't crash if /data doesnt exist

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,8 @@ pub mod voice_message;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // if we are inside docker, change cwd to /data
     if std::env::var("INSIDE_DOCKER").is_ok_and(|x| x != "0") {
-        std::env::set_current_dir("/data").expect("Failed to change working directory to /data");
+        // ignore if it doesn't exist
+        let _ = std::env::set_current_dir("/data");
     }
 
     let config = match Config::new() {
