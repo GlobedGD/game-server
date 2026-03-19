6edd007d621354480063fd08933a4779ab5d446b
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-03-19 12:09:25 +0100

    i forgot the test code

--- src/main.rs
+++ src/main.rs
@@ -101,7 +101,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut builder = server_shared::qunet::server::Server::builder()
         .with_memory_options(make_memory_limits(config.memory_usage))
-        // .with_max_messages_per_second(mlimit)
+        .with_max_messages_per_second(mlimit)
         .with_compression_determinator(make_compression_func(config.compression_level))
         .with_app_handler(handler);
 
